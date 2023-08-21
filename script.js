9986df1ea31db9ce97d4ad7b3dc0149356b6011a
warnikchow <31306002+warnikchow@users.noreply.github.com>
2023-08-21 23:01:47 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-<script>
-    function generateParagraph() {
-        var inputText = document.getElementById('inputText').value;
-        var preparedMessage = "some prepared message "; // You can modify this message
-        var outputParagraph = preparedMessage + inputText;
-        document.getElementById('output').value = outputParagraph;
-    }
-</script>
